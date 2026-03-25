836765eb492621fba93a6de884c8608075f12f3c
Chris <Britten63@hotmail.com>
2026-03-25 16:26:19 -0300

    changed pool and server config

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,7 @@
-import "dotenv/config";
-import app from "./src/app.js";
-
-console.log("DB:", process.env.DATABASE_URL);
-
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-require("dotenv").config();
+
+import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server running on port ${PORT}`),
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
